305d5e5e60fd00656bf9506e8fb7515e1972edda
dvranga <dvranga@users.noreply.github.com>
2020-12-13 02:10:19 +0530

    [ranganath]Added:Extend the servlet to accept a valid password

--- src/main/java/com/bridgelabz/myFirstServlet/LoginServlet.java
+++ src/main/java/com/bridgelabz/myFirstServlet/LoginServlet.java
@@ -22,6 +22,7 @@ import java.util.regex.Pattern;
 )
 public class LoginServlet extends HttpServlet {
     private static final String FIRST_NAME_PATTERN="^[A-Z]{1}[a-zA-Z]{2}[a-zA-z0-9]*";
+    private static final String PASSWORD_PATTERN="^([a-zA-Z0-9@*#]{8,15})$";
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -29,9 +30,12 @@ public class LoginServlet extends HttpServlet {
         boolean validateFirstName = validateFirstName(user);
         boolean checkFirstName = checkFirstName(request, response, validateFirstName);
         String pwd=request.getParameter("pwd");
+        boolean validatePassword = validatePassword(pwd);
+        boolean checkPassword = checkPassword(request, response, validatePassword);
+        System.out.println("validatePassword "+validatePassword+" "+checkPassword);
         String userID=getServletConfig().getInitParameter("user");
         String password=getServletConfig().getInitParameter("password");
-        if (checkFirstName==true) {
+        if (checkFirstName==true&&checkPassword==true) {
             if (userID.equals(user) && password.equals(pwd)) {
                 request.setAttribute("user", user);
                 request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
@@ -59,6 +63,24 @@ public class LoginServlet extends HttpServlet {
 
     public boolean validateFirstName(String firstName) {
         Pattern check= Pattern.compile(FIRST_NAME_PATTERN);
+        boolean value=check.matcher(firstName).matches();
+        return value;
+    }
+
+    private boolean checkPassword(HttpServletRequest request, HttpServletResponse response, boolean validateFirstName)
+            throws IOException, ServletException {
+        if (validateFirstName==false){
+            RequestDispatcher rd=getServletContext().getRequestDispatcher("/login.jsp");
+            PrintWriter out=response.getWriter();
+            out.println("<font color=red>Password is Incorrect</font>");
+            rd.include(request,response);
+            return false;
+        }
+        return true;
+    }
+
+    public boolean validatePassword(String firstName) {
+        Pattern check= Pattern.compile(PASSWORD_PATTERN);
         boolean value=check.matcher(firstName).matches();
         return value;
     }
